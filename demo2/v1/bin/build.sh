e459f8d83f8e50d8d99a49a1ab3b7356aa2e3307
huiwen <huiwen.lee@oracle.com>
2026-02-25 14:27:28 +0800

    update dockerfiles and build

--- demo2/v1/bin/build.sh
+++ demo2/v1/bin/build.sh
@@ -2,6 +2,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 . $SCRIPT_DIR/../../../bin/env.sh
 
 ./mvnw clean package
-docker build -t webquerydb:v1 .
+docker buildx build --platform linux/amd64,linux/arm64 -t webquerydb:v1 .
 docker images | grep webquerydb
 
